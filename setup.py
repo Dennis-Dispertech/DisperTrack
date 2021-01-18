4e1e0616d6bddc5e623f7aedf50294d38e7a0c9f
Aquiles Carattino <aqui.carattino@gmail.com>
2021-01-18 17:00:16 +0100

    Solving problem with overwriting data

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DisperTrack',
+    version='1.0',
+    packages=['dispertrack', 'dispertrack.view', 'dispertrack.model'],
+    url='https://www.dispertech.com',
+    license='',
+    author='Aquiles Carattino',
+    author_email='carattino@dispertech.com',
+    description='Software suit to track and analyse nanoparticles'
+)
