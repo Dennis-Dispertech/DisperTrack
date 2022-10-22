1e1fe56ee2cb4412ebfa4fb612138bf9ffab73d7
Aquiles Carattino <aqui.carattino@gmail.com>
2022-10-22 17:52:31 +0200

    Added slider to show waterfall in parts, added histogram and scatter plot. It is till WIP

--- dispertrack/start_waterfall_analysis.py
+++ dispertrack/start_waterfall_analysis.py
@@ -1,11 +1,16 @@
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 import os
+
+from dispertrack.view import view_folder
+
 os.environ['QT_MAC_WANTS_LAYER'] = '1'
 from dispertrack.view.waterfall_window import WaterfallWindow
 
 
 def start_analysis():
     app = QApplication([])
+    app.setWindowIcon(QIcon(str(view_folder / 'GUI'/ 'favicon.png')))
     win = WaterfallWindow()
     win.show()
     app.exec()
--- dispertrack/view/histograms_window.py
+++ dispertrack/view/histograms_window.py
@@ -12,35 +12,25 @@ class HistogramWindow(QMainWindow):
         super(HistogramWindow, self).__init__()
         uic.loadUi(view_folder / 'GUI' / 'histograms_sizes.ui', self)
         self.analyze_model = analyze_model
-        self.analyze_model.calculate_particle_properties()
+        self.d = [2*self.analyze_model.pcle_data[i].get('r', np.nan) for i in self.analyze_model.pcle_data.keys()]
+        self.i = [self.analyze_model.pcle_data[i].get('mean_intensity', np.nan) for i in self.analyze_model.pcle_data.keys()]
+
 
         self.update_histogram_diffusion()
         self.update_histogram_intensities()
         self.update_plot_diffusion_intensity()
 
     def update_histogram_diffusion(self):
-        d = 2 * self.analyze_model.r / 1e-9
-        # y, x = np.histogram(self.analyze_model.r, bins=np.linspace(np.min(self.analyze_model.r),
-        #                                                            np.max(self.analyze_model.r), 40))
-        y, x = np.histogram(d, bins=np.linspace(np.min(d), np.max(d), 40))
-        # y, x = np.histogram(self.analyze_model.d, bins=np.linspace(np.min(self.analyze_model.d),
-        #                                                            np.max(self.analyze_model.d), 40))
-        self.analyze_model.x_y = (x, y)
-        self.diffusion_histogram.plot(x, y, stepMode=True, fillLevel=0, brush=(0,0,255,150))  # this shows incorrect x-axis
-        # self.diffusion_histogram.plot(x, y, stepMode='center', fillLevel=0, brush=(0,0,255,150))  # this gives an error
-        # self.diffusion_histogram.plot(np.repeat(x, 2)[1:-1], np.repeat(y, 2), fillLevel=0, brush=(0, 0, 255, 150))  # making it "manually" results in the exact same incorrect x-axis
-        # self.diffusion_histogram.plot((x[:-1] + x[1:]) / 2, y, fillLevel=0, brush=(0, 0, 255, 150))  # making it "manually" results in the exact same incorrect x-axis
-        # plt.hist(d, 40)
-        self.diffusion_histogram.setTitle('particle size histogram')
+        plt.figure()
+        plt.hist(self.d, 40)
+        plt.show()
 
     def update_histogram_intensities(self):
-        y, x = np.histogram(self.analyze_model.mean_intensity, bins=np.linspace(np.min(
-            self.analyze_model.mean_intensity),
-                            np.max(self.analyze_model.mean_intensity), 40))
-
-        self.intensity_histogram.plot(x, y, stepMode=True, fillLevel=0, brush=(0,0,255,150))
-        self.intensity_histogram.setTitle('intensity histogram')
+        plt.figure()
+        plt.hist(self.i, 40)
+        plt.show()
 
     def update_plot_diffusion_intensity(self):
-        self.diffusion_intensity_plot.plot(self.analyze_model.mean_intensity, self.analyze_model.r, symbol ='o')
-        self.diffusion_intensity_plot.setTitle('"diffusion intensity plot"')
+        plt.figure()
+        plt.plot(self.d, self.i)
+        plt.show()
